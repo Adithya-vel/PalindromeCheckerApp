bde924da28348906f329c820f4a2e3aa0a067cc6
Adithya-vel <adithyamuralikrishnan512@gmail.com>
2026-03-11 15:31:43 +0530

    UC5: Stack based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,23 +1,29 @@
-// UC5 — Stack Based Palindrome
-String wordUC5 = "radar";
+public class PalindromeCheckerApp {
 
-java.util.Stack<Character> stack = new java.util.Stack<>();
+    public static void main(String[] args) {
+
+        // UC5 — Stack Based Palindrome
+        String wordUC5 = "radar";
+
+        java.util.Stack<Character> stack = new java.util.Stack<>();
 
 // Push characters
-for (int i = 0; i < wordUC5.length(); i++) {
-        stack.push(wordUC5.charAt(i));
+        for (int i = 0; i < wordUC5.length(); i++) {
+            stack.push(wordUC5.charAt(i));
         }
 
 // Build reversed word using stack
-String reversed = "";
+        String reversed = "";
 
-while (!stack.isEmpty()) {
-reversed += stack.pop();
-}
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
 
 // Compare
         if (wordUC5.equals(reversed)) {
-        System.out.println(wordUC5 + " is a Palindrome (Stack Method)");
-} else {
-        System.out.println(wordUC5 + " is NOT a Palindrome (Stack Method)");
+            System.out.println(wordUC5 + " is a Palindrome (Stack Method)");
+        } else {
+            System.out.println(wordUC5 + " is NOT a Palindrome (Stack Method)");
+        }
+
 }
